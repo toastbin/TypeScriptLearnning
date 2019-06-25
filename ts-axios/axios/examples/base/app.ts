f2d24a06cc1a280de596eebd62bf641d7a207198
toast <729440390@qq.com>
2019-06-26 00:26:49 +0800

    axios

--- ts-axios/axios/examples/base/app.ts
+++ ts-axios/axios/examples/base/app.ts
@@ -52,5 +52,22 @@ axios({
   data: {
     a:1,
     b:2
+  },
+  headers:{
+    'Content-Type': 'application/json;charset=utf-8',
+    'Accept': 'application/json, text/plain, */*'
   }
+}).then(res=>{
+  console.log(res);
+})
+
+
+const paramsStr = 'a=URL.search&b=url'
+const searchParams = new URLSearchParams(paramsStr)
+axios({
+  method: 'post',
+  url: '/base/post',
+  data: searchParams
+}).then(res=>{
+  console.log(res);
 })
--- ts-axios/axios/src/xhr.ts
+++ ts-axios/axios/src/xhr.ts
@@ -1,12 +1,37 @@
-import { AxiosRequestConfig } from './types'
-export default function xhr(config: AxiosRequestConfig): void {
-  const { data = null, url, method = 'get', headers } = config
-  const request = new XMLHttpRequest()
+import { AxiosRequestConfig, AxiosPromise, AxiosResponse } from './types'
+import { parseHeaders } from './helpers/headers'
+export default function xhr(config: AxiosRequestConfig): AxiosPromise {
+  return new Promise((resolve, reject)=>{
+    const { data = null, url, method = 'get', headers, responseType } = config
+    const request = new XMLHttpRequest()
+    if(responseType){
+      request.responseType = responseType
+    }
+    request.open(method.toUpperCase(), url, true)
 
-  request.open(method.toUpperCase(), url, true)
-  Object.keys(headers).forEach(name => {
-    if (data === null && name.toLowerCase() === 'content-type') delete headers[name]
-    else request.setRequestHeader(name, headers[name])
+    request.onreadystatechange = ()=>{
+      if(request.readyState !== 4){
+        return 
+      }
+      const responseHeader = parseHeaders(request.getAllResponseHeaders())
+      const responseData = responseType !== 'text' ? request.response : request.responseText
+      const response: AxiosResponse = {
+        data: responseData,
+        status: request.status,
+        statusText: request.statusText,
+        headers: responseHeader,
+        config,
+        request
+      }
+      resolve(response)
+    } 
+
+    Object.keys(headers).forEach(name => {
+      if (data === null && name.toLowerCase() === 'content-type') delete headers[name]
+  
+      else request.setRequestHeader(name, headers[name])
+    })
+    request.send(data)
+
   })
-  request.send(data)
 }
